34dacfc153d5f719cb7b67a82b8247fa533996e0
JqkerN-laptop <ilian.corneliussen@gmail.com>
2019-12-05 13:56:02 +0100

    Added dilated

---
+++ Functions/binary_dialated.m
@@ -0,0 +1,7 @@
+function [outFrame] = binary_dialated(Frame,inc)
+%BINARY_DIALATED Summary of this function goes here
+%   Detailed explanation goes here
+kernel = ones(inc,inc);
+Frame(Frame == 1 )
+end
+
--- Functions/blurr.m
+++ Functions/blurr.m
@@ -2,7 +2,9 @@ function [outFrame] = blurr(Frame,sigma)
 %BLURR Summary of this function goes here
 %   Detailed explanation goes here
 
-
+d = 2*ceil(sigma*2) + 1;
+h = fspecial('gaussian', [d d], sigma);
+outFrame = imfilter(Frame, h);
 
 end
 
