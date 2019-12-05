444eb90ac830165b13259c6c62e3009306c77524
andrejwilczek <andrej.wilczek@gmail.com>
2019-12-05 11:50:40 +0100

    red dot id

--- Cv_test.m
+++ Cv_test.m
@@ -1,6 +1,37 @@
+clc
+clear all
+
+RGB = [236,0,51];
+
+japan = imread('japan.jpg');
+size(japan)
+figure(1)
+imshow(japan)
+H = size(japan,1);
+W = size(japan,2);
+
+japan = reshape(japan,H*W,3);
 
 
-%RGB (236,0,51)
+mask = zeros(W*H,1);
+tol = 10; 
+
+
+dist = pdist2(japan,RGB);
+
+mask(dist<tol) = 1;
+
+mask = reshape(mask,H,W,1);
+figure(2)
+imshow(mask)
+
+
+
+
+
+
+
+
 
 
 
