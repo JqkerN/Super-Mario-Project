105b1f9d13ca3aa03de3cac6faea04504e58d192
Andrej Wilczek <andrejw@kth.se>
2019-12-06 16:56:49 +0100

    Added a working PF

--- ParticleFilter/predict.m
+++ ParticleFilter/predict.m
@@ -1,5 +1,9 @@
-function S_bar = predict(S,bounds,N)
+function S_bar = predict(Sigma_R)
+global N S 
 % Calculates an estimated state 
-S_bar = S + randn(size(bounds,1),N).*repmat(sqrt(diag(Sigma_R)),1,N);
+diff = randn(2,N).*repmat(sqrt(diag(Sigma_R)),1,N);
+diff = [diff(1,:);diff(2,:)];
+S_bar = S(1:2,:)+diff; %+ randn(size(lim,1),N).*repmat(sqrt(diag(Sigma_R)),1,N);
+
 % sqrt?
 end
